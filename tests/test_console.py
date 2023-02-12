ad1220d7c69753a165b0fb1e8e7d2b620468ec70
1Ronney <ronnywinga@gmail.com>
2023-02-11 18:43:54 -0800

    console

---
+++ tests/test_console.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Unittest defination for console.py
+Unittetst classes:
+    TestHBNBCommand_prompting
+    TestHBNBCommand_help
+    TestHBNBCommand_exit
+    TestHBNBCommand_create
+    TestHBNBCommand_show
+    TestHBNBCommand_all
+    TestHBNBCommand_destroy
+    TestHBNBCommand_update
+"""
